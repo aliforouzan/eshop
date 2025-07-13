b56de785c99e1e7754ae425628e8013e269481cb
Ali Forouzan <ali.forouzan@partdp.ir>
2025-07-13 17:03:51 +0330

    Add abstraction for DDD in Domain Layer

---
+++ Src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,22 @@
+namespace Ordering.Domain.Abstractions;
+
+public abstract class Aggregate<T> : Entity<T>, IAggregate<T>
+{
+    private readonly List<IDomainEvent> _domainEvents = [];
+    
+    public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+    
+    public void CreateDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public IDomainEvent[] ClearDomainEvents()
+    {
+        var dequeuedEvents = _domainEvents.ToArray();
+        
+        _domainEvents.Clear();
+
+        return dequeuedEvents;
+    }
+}

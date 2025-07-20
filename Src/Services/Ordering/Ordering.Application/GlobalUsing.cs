a6f5c67fb093421b8e5b29418d0e0bc310190863
Ali Forouzan <ali.forouzan@partdp.ir>
2025-07-20 16:27:00 +0330

    Add create order command handler with using dbcontext interface in application layer

--- Src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ Src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -1,2 +1,9 @@
+global using BuildingBlocks.CQRS;
+
 global using Ordering.Domain.Enums;
 global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
+global using Ordering.Domain.ValueObjects.StronglyTypes;
+
+global using Ordering.Application.Dtos;
+global using Ordering.Application.Data;
--- Src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
+++ Src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -1,11 +1,45 @@
-using BuildingBlocks.CQRS;
-
 namespace Ordering.Application.Orders.Commands.CreateOrder;
 
-public class CreateOrderHandler : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+public class CreateOrderHandler(IApplicationDbContext dbContext) 
+    : ICommandHandler<CreateOrderCommand, CreateOrderResult>
 {
-    public Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    public async Task<CreateOrderResult> Handle(CreateOrderCommand command, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var order = CreateOrder(command.Order);
+        dbContext.Orders.Add(order);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new CreateOrderResult(order.Id.Value);
+    }
+
+    private Order CreateOrder(OrderDto orderDto)
+    {
+        var shippingDto = orderDto.ShippingAddress;
+        var shipping = Address.Of(shippingDto.FirstName, shippingDto.LastName, shippingDto.EmailAddress,
+            shippingDto.AddressLine, shippingDto.Country, shippingDto.State, shippingDto.ZipCode);
+
+        var billingDto = orderDto.BillingAddress;
+        var billing = Address.Of(billingDto.FirstName, billingDto.LastName, billingDto.EmailAddress,
+            billingDto.AddressLine, billingDto.Country, billingDto.State, billingDto.ZipCode);
+
+        /*
+         TODO: check this: is it possible?
+         billing = billingDto.Adapt<Address>();
+        */
+
+        var paymentDto = orderDto.Payment;
+        var payment = Payment.Of(paymentDto.CardName, paymentDto.CardNumber, paymentDto.Expiration, paymentDto.Cvv2,
+            paymentDto.PaymentMethod);
+
+        var order = Order.Create(
+            OrderId.Of(orderDto.Id),
+            CustomerId.Of(orderDto.CustomerId),
+            OrderName.Of(orderDto.OrderName),
+            shipping,
+            billing,
+            payment
+        );
+
+        return order;
     }
 }

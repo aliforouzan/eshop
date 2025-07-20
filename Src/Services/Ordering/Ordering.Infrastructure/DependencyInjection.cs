7d07af7d53b3c831749adbeb140ef95959b88e0f
Ali Forouzan <ali.forouzan@partdp.ir>
2025-07-20 08:45:35 +0330

    add SaveChangesInterceptor to Auditing Entities for Order Entities

--- Src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ Src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Infrastructure.Data.Interceptors;
 
 namespace Ordering.Infrastructure;
 
@@ -10,7 +11,11 @@ public static class DependencyInjection
     {
         var connectionString = configuration.GetConnectionString("Database");
 
-        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.AddInterceptors(new AuditableEntityInterceptor());
+            options.UseSqlServer(connectionString);
+        });
 
         return services;
     }
--- Src/Services/Ordering/Ordering.Infrastructure/globalUsing.cs
+++ Src/Services/Ordering/Ordering.Infrastructure/globalUsing.cs
@@ -1,5 +1,6 @@
 global using Ordering.Infrastructure.Data;
 global using Microsoft.EntityFrameworkCore;
 global using Ordering.Domain.Models;
-global using Ordering.Domain.ValueObjects.StronglyTypes;
 global using Ordering.Domain.Enums;
+global using Ordering.Domain.Abstractions;
+global using Ordering.Domain.ValueObjects.StronglyTypes;
